ad4629ad032e122ff26b711ce521592752fb39e1
Emmanuel Gyang <emmanuel.gyang@hubtel.com>
2024-07-02 23:10:29 +0000

    initializing project

---
+++ user_service/src/app.ts
@@ -0,0 +1,32 @@
+// templates/app.js
+
+// eslint-disable-next-line import/no-unresolved
+import express, {Application, Response, Request} from 'express';
+import cors from 'cors';
+import config from 'config';
+import { connectDatabase } from './helper/database.js';
+import logger from './helper/logger';
+
+const appUrl: string = config.get('appUrl');
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+  })
+);
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello World🤩🤗',
+    info: 'https://www.npmjs.com/package/@codetanna/nex-cli',
+  });
+});
+connectDatabase();
+
+app.listen(3000, () => {
+  logger.info(`Server is running on ${appUrl} 😎...`);
+});
---
+++ user_service/src/helper/database.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from 'sequelize';
+import logger from '../helper/logger';
+import config from 'config';
+
+const databaseUrl = config.get('dbUrl');
+
+const sequelize = new Sequelize(databaseUrl);
+
+export const connectDatabase = async () => {
+    try {
+    await sequelize.authenticate();
+    logger.info('Connection has been established successfully.');
+  } catch (error) {
+    logger.error('Unable to connect to the database:', error);
+  }
+}
